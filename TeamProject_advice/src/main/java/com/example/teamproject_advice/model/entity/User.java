ea15a3683d50cb800d3da02a6e5d98610ad7c5ed
gurygury5 <83051136+gurygury5@users.noreply.github.com>
2021-08-16 19:06:09 +0900

    210816 user code update

--- TeamProject_advice/src/main/java/com/example/teamproject_advice/model/entity/User.java
+++ TeamProject_advice/src/main/java/com/example/teamproject_advice/model/entity/User.java
@@ -1,4 +1,34 @@
 package com.example.teamproject_advice.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String account;
+    private String password;
+    private String status;
+    private String phoneNumber;
+    private LocalDateTime registered_at;
+    private LocalDateTime unregistered_at;
+
+    private LocalDateTime created_at;
+    private String created_by;
+    private LocalDateTime updated_at;
+    private String updated_by;
+
 }
