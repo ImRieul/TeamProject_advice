13aa575153ae37b3ccf55dddd28e12fc95718792
gurygury5 <83051136+gurygury5@users.noreply.github.com>
2021-08-16 17:42:30 +0900

    add project file

---
+++ TeamProject_advice/src/main/java/com/example/teamproject_advice/TeamProjectAdviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamproject_advice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectAdviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectAdviceApplication.class, args);
+    }
+
+}
---
+++ TeamProject_advice/src/test/java/com/example/teamproject_advice/TeamProjectAdviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamproject_advice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectAdviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
