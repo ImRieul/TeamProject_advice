4aaeb7058fd32fc1ade8c79f7fcaf4608810f516
mojja-im <82865792+mojja-im@users.noreply.github.com>
2021-09-06 11:33:31 +0900

    210906 add board notes

--- TeamProject_advice/src/main/java/com/example/teamproject_advice/controller/BoardController.java
+++ TeamProject_advice/src/main/java/com/example/teamproject_advice/controller/BoardController.java
@@ -18,12 +18,14 @@ import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
-@Controller
+@Controller                         // controller임을 명시(spring boot), 학원 : 홈페이지 > 공부 참고 참조
 @RequestMapping("/board")
 public class BoardController {
 
     private final BoardService service;
 
+    // /board/list로 연결됨
+    // parameter로는 search, Model, Pageable( page, size, sort )가 있음
     @GetMapping("/list")
     public String list(@RequestParam(value = "search", required = false) String search,
                        Model model, Pageable pageable) {
@@ -31,39 +33,43 @@ public class BoardController {
         //         검색한 내용이 없을 때 (search param이 없을 때)
         Page<Board> boardPage = ( search.isEmpty() )? service.boardListPage(pageable) : service.searchBoardPage(search, pageable);
 
-        model.addAttribute("list", boardPage);                          // page list를 전송
-        model.addAttribute("paging", service.paging(pageable));         // 페이지 번호를 위한 list
-        model.addAttribute("page", service.checkPageable(pageable));    // 현재 페이지 정보 detail에 전달용
-        model.addAttribute("lastPageNumber", service.boardListLastPage(pageable.getPageSize()) -1);   // 마지막 페이지 확인용
+        // model(return) 에 값을 전송
+        model.addAttribute("list", boardPage);                          // list(key), boardPage(value, Page type)
+        model.addAttribute("paging", service.paging(pageable));         // paging(key), 페이지 번호를 위한 list(value, List type)
+        model.addAttribute("page", service.checkPageable(pageable));    // page(key), 현재 페이지 정보 detail에 전달용(value, int type)
+        model.addAttribute("lastPageNumber", service.boardListLastPage(pageable.getPageSize()) -1);   // lastPageNumber(key), 마지막 페이지 확인용(value, int type)
 
-        model.addAttribute("search", search);
+        model.addAttribute("search", search);       // search(key), value String type
 
-        return "board/list";
+        return "board/list";        // board/list.html로 연결
     }
 
+    // /board/detail로 연결됨
+    // parameter로는 id, size, Model이 있음
     @GetMapping("/detail")
     public String detail(@RequestParam("id") Long id,
                          @RequestParam("size") int size,
                          Model model) {
 
-        Board board = service.findById(id);
-        model.addAttribute("board", board);
+        Board board = service.findById(id);                 // id로 검색해서 테이블의 행 가져오기
+        model.addAttribute("board", board);     // board(key), board(value, Board type)
 
-        model.addAttribute("boardNext", service.findById(id+1));        // 다음 글
-        model.addAttribute("boardPrev", service.findById(id-1));        // 이전 글
+        // model(return) 에 값을 전송
+        model.addAttribute("boardNext", service.findById(id+1));        // boardNext(key), 다음 글(value, board type)
+        model.addAttribute("boardPrev", service.findById(id-1));        // boardPrev(key), 이전 글(value, board type)
 
         // 목록으로
-        model.addAttribute("page", service.returnPageNumber(id, size));
-        model.addAttribute("size", size);
+        model.addAttribute("page", service.returnPageNumber(id, size)); // page(key), 목록 번호(value, int type)
+        model.addAttribute("size", size);                               // size(key), page 설정 size(value, int type)
 
-        // 댓글 테스트
+        // 댓글 테스트용
         Map<String, String> testMap = new HashMap<>();
 
         testMap.put("registeredAt", "2021-09-01 12:00");
         testMap.put("comment", "테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 테스트 내용입니다 ");
         testMap.put("nickname", "멍청한비둘기들");
 
-        model.addAttribute("comment", testMap);
+        model.addAttribute("comment", testMap);     // comment(key), testMap(value, May<String, String> type)
 
         return "board/detail";
     }
